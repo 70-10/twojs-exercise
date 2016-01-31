15112614529a20e2a22fdb6b25e2c4503e9949e4
70_10 <chiclown.dev@gmail.com>
2016-01-31 16:48:17 +0900

    [mod] two.js introduction

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
 import Two from "two.js";
 
+const app = document.getElementById("app");
+
 const two = new Two({
   fullscreen: true,
   autostart: true
-}).appendTo(document.getElementById("app"));
+}).appendTo(app);
 
-const rect = two.makeRectangle(two.width / 2, two.height / 2, 50, 50);
+const circle = two.makeCircle(-70, 0, 50);
+circle.fill = "#FF8000";
+circle.stroke = "orangered";
 
-two.bind("update", () => {
-  rect.rotation += 0.05;
-});
+const rect = two.makeRectangle(70, 0, 100, 100);
+rect.fill = "rgba(0, 200, 255, 0.75)";
+rect.stroke = "#1C75BC";
+
+const group = two.makeGroup(circle, rect);
+group.translation.set(two.width / 2, two.height / 2);
+group.scale = 0;
+group.noStroke();
+
+two.bind("update", frameCount => {
+  if (group.scale > 0.9999) {
+    group.scale = group.rotation = 0;
+  }
+  const t = (1 - group.scale) * 0.125;
+  group.scale += t;
+  group.rotation += t * 4 * Math.PI;
+}).play();
